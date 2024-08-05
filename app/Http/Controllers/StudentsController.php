d334ffdaf33835244dc2f79d8196c621b20a094d
liminghin <machinelearninglesson@gmail.com>
2024-08-05 20:31:57 +0700

    Students Index

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -14,6 +14,8 @@ class StudentsController extends Controller
     public function index()
     {
         //
+        $students = Students::all();
+        return view('students.index', compact('students'));
     }
 
     /**
